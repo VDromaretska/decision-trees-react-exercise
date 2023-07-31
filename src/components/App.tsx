0685dee9cfb2a39bf57c2bb2ec1d83ef51b60879
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-07-31 12:03:28 +0200

    App works

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,44 @@
 import "./App.css";
 import createTree from "../exampleTrees/animalsQuizTree";
+import { QuestionNode } from "../tree";
+import { useState, useEffect } from "react";
 
 export default function App() {
-    const tree = createTree();
+    const [currentTree, setCurrentTree] = useState(createTree());
+    const [currentQuestion, setCurrentQuestion] = useState<string>("question");
+
+    useEffect(() => {
+        if (currentTree.kind === "question") {
+            setCurrentQuestion(currentTree.question);
+        }
+    }, [currentTree]);
+
+    const handleClickYes = (currentTree: QuestionNode) => {
+        setCurrentTree(currentTree.yesSubtree);
+    };
+    const handleClickNo = (currentTree: QuestionNode) => {
+        setCurrentTree(currentTree.noSubtree);
+    };
+
     return (
         <main className="App">
             Think of an animal from this list and I will try to guess it!
-            <div>{tree.kind}</div>
-            <button>Restart</button>
+            <p>sea otter, grizzly bear, bald eagle, penguin, snake</p>
+            {currentTree.kind === "question" && (
+                <div>
+                    <p>{currentQuestion}</p>
+                    <button onClick={() => handleClickYes(currentTree)}>
+                        Yes
+                    </button>
+                    <button onClick={() => handleClickNo(currentTree)}>
+                        No
+                    </button>
+                </div>
+            )}
+            {currentTree.kind === "leaf" && <p>{currentTree.result}</p>}
+            <button onClick={() => setCurrentTree(createTree())}>
+                Restart
+            </button>
         </main>
     );
 }
